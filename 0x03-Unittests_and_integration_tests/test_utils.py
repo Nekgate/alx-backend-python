9add187ea75854fd61da09f4c46cbdeae1fd2d3c
Nekgate <Nekgate@gmail.com>
2024-07-27 19:25:20 +0100

    Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,7 +6,8 @@ to test multiple cases.
 """
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from unittest.mock import patch, Mock 
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -30,6 +31,33 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as cm:
             access_nested_map(nested_map, path)
         self.assertEqual(str(cm.exception), repr(expected_key))
+
+
+class TestGetJson(unittest.TestCase):
+    """ Class to test the get_json function."""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """ Test that get_json returns the expected
+        result with mocked requests.
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        # Call the function under test
+        result = get_json(test_url)
+
+        # Check that requests.get was called once with the test_url
+        mock_get.assert_called_once_with(test_url)
+
+        # Check that the result matches the test_payload
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == "__main__":
